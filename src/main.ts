b273bb17ea139df86b9dab5dfa5e64f6d868fbe4
James Andersen <james.andersen@universalmind.com>
2016-04-16 00:16:46 -0500

    Further work on styling; some basic interactivity

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ require('./styles.less');
 var last_known_scroll_position : number = 0;
 var ticking : boolean = false;
 var header : Element;
+var mainMenu : Element = document.getElementById("main-menu");
 
 function doSomething(scroll_pos) {
   // do something with the scroll position
   if(!header) header = document.getElementById("header");
-  if (last_known_scroll_position > 40) {
+  if (last_known_scroll_position > 200) {
      header.classList.add("docked");
   } else {
      header.classList.remove("docked");
+     mainMenu.classList.remove("open");
   }
 }
 
@@ -27,3 +29,17 @@ window.addEventListener("scroll", (ev: UIEvent) => {
    }
    ticking = true;
 });
+
+mainMenu.addEventListener("click", (ev: UIEvent) => {
+   mainMenu.classList.toggle("open");
+});
+
+var checkboxes = mainMenu.querySelectorAll("input[type=checkbox]");
+for(var i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("change", onCheckboxChange);
+}
+
+function onCheckboxChange(ev : UIEvent) {
+  document.body.classList.toggle(ev.target.value);
+  ev.stopImmediatePropagation();
+}
